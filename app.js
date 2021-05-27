441261c6141aff66fca9263eb29eb4c81bbb3314
GorreriFranco <franco.gorreri@gmail.com>
2021-05-27 01:28:48 -0300

    Middleware setup and app.use introd

---
+++ app.js
@@ -0,0 +1,27 @@
+const app = require('express')()
+const logger = require('./logger')
+// req => middleware => res
+
+app.use('/api', logger)
+// if you omit the path it's going to be applied to all the request BELOW
+// more info about app.use() in https://expressjs.com/en/4x/api.html#app.use
+
+app.get('/', (req, res)=>{
+  res.send('Home')
+})
+
+app.get('/about', (req, res)=>{
+  res.send('About')
+})
+
+app.get('/api/products', (req, res)=>{
+  res.send('Products Resourse!')
+})
+
+app.get('/api/items', (req, res)=>{
+  res.send('Items Resourse!')
+})
+
+app.listen(5000, ()=>{
+  console.log('Server is listening on port 5000...');
+})
